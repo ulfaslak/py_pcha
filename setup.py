6d0fe7c70997f325cf000848d02ee1e59c4121a9
ulfaslak <ulfjensen@gmail.com>
2016-03-26 02:48:22 +0300

    second commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='py_pcha',
+    version='0.1',
+    description='Python implemenation of PCHA algorithm',
+    url='https://github.com/ulfaslak/py_pcha',
+    author='Ulf Aslak',
+    author_email='ulfjensen@gmail.com',
+    license='MIT',
+    packages=['py_pcha'],
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ]
+)
