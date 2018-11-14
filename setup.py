7609f1858544dfc761ba4a8ae161e3eb66937f10
Christophe <christophe@tactile.dk>
2018-11-14 15:35:22 +0000

    Incremented version and removed wrong download_url

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='py_pcha',
-    version='0.1.2',
+    version='0.1.3',
     description='Python implemenation of PCHA algorithm',
     url='https://github.com/ulfaslak/py_pcha',
-    download_url = 'https://github.com/ulfaslak/py_pcha/tarball/0.1',
+    # download_url = 'https://github.com/ulfaslak/py_pcha/tarball/0.1',
     author='Ulf Aslak',
     author_email='ulfjensen@gmail.com',
     license='MIT',
